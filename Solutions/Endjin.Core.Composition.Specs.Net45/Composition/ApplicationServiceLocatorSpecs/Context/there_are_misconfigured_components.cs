e2cc468dd001d4f0de60d2e02bbc854eb83552d1
mwadams <matthew.adams@endjin.com>
2013-12-06 17:07:54 +0000

    Missing from commit.

---
+++ Solutions/Endjin.Core.Composition.Specs.Net45/Composition/ApplicationServiceLocatorSpecs/Context/there_are_misconfigured_components.cs
@@ -0,0 +1,22 @@
+﻿namespace Endjin.Core.Specs.Composition.ApplicationServiceLocatorSpecs.Context
+{
+    #region Using statements
+
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Endjin.Core.Composition;
+    using Endjin.Core.Container;
+    using Endjin.Core.Specs.Composition.Context;
+    using Endjin.Core.Specs.Helpers;
+
+    #endregion
+
+    public class there_are_misconfigured_components : SharedContext<ApplicationServiceLocator>
+    {
+        protected override void Initialize(ITestStateWithContext<ApplicationServiceLocator> state)
+        {
+            var container = state.GetMockFor<IContainer>();
+            container.SetupGet(c => c.MisconfiguredComponents).Returns(new ReadOnlyCollection<ComponentRegistration>(new List<ComponentRegistration> { new ComponentRegistration() }));
+        }
+    }
+}
