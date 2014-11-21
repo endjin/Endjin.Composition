e861b0409ff383269016033d5ddacb35cd33e10c
Matthew Adams <matthew.adams@endjin.com>
2014-11-21 14:36:56 +0000

    Added the ContentFactoryInstallerBase to Composition.

---
+++ Solutions/Endjin.Core.Composition/Installers/ContentFactoryInstallerBase.cs
@@ -0,0 +1,30 @@
+﻿namespace Endjin.Core.Installers
+{
+    #region Using Directives
+
+    using Endjin.Core.Container;
+
+    #endregion 
+
+    public abstract class ContentFactoryInstallerBase<T, TInterface> : IInstaller 
+        where T : class, TInterface, new()        
+    {
+        public void Install(IContainer container)
+        {
+            TInterface contentFactory = new T();
+
+            if (container.TryResolve<TInterface>() == null)
+            {
+                container.Register(Component.For<TInterface>().ImplementedBy(contentFactory).AsSingleton());
+            }
+            else
+            {
+                contentFactory = container.Resolve<TInterface>();
+            }
+
+            this.RegisterDefaultContent(contentFactory);
+        }
+
+        protected abstract void RegisterDefaultContent(TInterface contentFactory);
+    }
+}
