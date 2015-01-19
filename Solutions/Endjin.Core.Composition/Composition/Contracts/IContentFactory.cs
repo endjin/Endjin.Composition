a7c3b772174c5da667b94f815b9c42939fa9014e
Matthew Adams <matthew.adams@endjin.com>
2015-01-19 15:45:57 +0000

    Created a non-generic interface for basic registration.

--- Solutions/Endjin.Core.Composition/Composition/Contracts/IContentFactory.cs
+++ Solutions/Endjin.Core.Composition/Composition/Contracts/IContentFactory.cs
@@ -2,14 +2,17 @@
 
 namespace Endjin.Core.Composition.Contracts
 {
-    public interface IContentFactory<T>
+    public interface IContentFactory
+    {
+        void RegisterContentFor(Type instanceType, string contentType);
+    }
+
+    public interface IContentFactory<T> : IContentFactory
     {
         T GetContentFor(string contentType);
 
         void RegisterContent(string contentType, T content);
 
         void RegisterContentFor<TInstance>(string contentType) where TInstance : T;
-
-        void RegisterContentFor(Type instanceType, string contentType);
     }
 }
